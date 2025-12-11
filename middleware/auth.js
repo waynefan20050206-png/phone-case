15f110a6ec3207ae73875574253e6119b8a62aa8
zawaqt <924542733@sfsu.edu>
2025-12-11 00:42:54 -0800

    Delete middleware directory

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-function requireLogin(req, res, next) {
-    if (!req.session.userId) {
-        req.session.error = "Please login first.";
-        return res.redirect("/login");
-    }
-    next();
-}
-
-module.exports = { requireLogin };
